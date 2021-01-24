53367d8274106685c33ccec9b963a61378bfddf9
Darshan B.S <darshan.b.s@TECH-LP-721.local>
2021-01-24 23:33:32 +0530

    changed .travis file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          goli soda
+          deepak is a pinnocio
         </p>
         <a
           className="App-link"
